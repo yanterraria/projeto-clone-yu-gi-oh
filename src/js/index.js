fa5d74ab9da91c82782f72211cf82c0d99cf744d
yanterraria <yanduarte012@gmail.com>
2025-01-27 12:53:51 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,45 @@
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+
+cartoes.forEach(cartao => {
+  cartao.addEventListener("click", function() {
+    const cartaVirada = cartao.querySelector(".carta-virada");
+
+    // virar o cartão
+    cartao.classList.toggle("virar");
+    // mostrar o fundo da carta
+    cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+    const descricao = cartao.querySelector(".descricao");
+    descricao.classList.toggle("esconder");
+  });
+});
+
+btnAvancar.addEventListener("click", function () {
+  if (cartaoAtual === cartoes.length - 1) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual++;
+  mostrarCartao(cartaoAtual);
+});
+
+btnVoltar.addEventListener("click", function () {
+  if (cartaoAtual === 0) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual--;
+  mostrarCartao(cartaoAtual);
+});
+
+function mostrarCartao(cartaoAtual) {
+  cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
+}
